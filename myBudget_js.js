17a55c7dc573a14ebad50dcea8c3be20d3a4e640
Ori assia <oriassia@gmail.com>
2024-05-08 16:51:32 +0300

    ori

--- myBudget_js.js
+++ myBudget_js.js
@@ -23,6 +23,7 @@ function loclStorgeUpdate() {
 function addIncomeItem(incomeArray, description, value) {
   const incomeObject = { description: description, value: value };
   incomeArray.push(incomeObject);
+  
   elememtIncomeList.innerElement += `<li class="list-item">
         <span class="item-name">${description}</span>
         <span class="item-value">${value}</span>
@@ -39,4 +40,28 @@ function addExpensesItem(expensesArray, description, value) {
         </li>`;
 }
 
-function updateBalance() {}
+function updateBalance() {
+
+}
+
+
+
+function updateBalanceSection(){
+    let incomeSum = computeArraySum(incomeArray)
+    let expensesSum = computeArraySum(expensesArray)
+
+    document.querySelector(".sum-container.income .value").textContent = `+ $ ${incomeSum}`
+
+    document.querySelector(".sum-container.income .value").textContent = `+ $ ${expensesSum}` 
+    
+    document.querySelector(".total-sum").textContent = `$ ${incomeSum - expensesSum}`;
+}
+
+
+function computeArraySum(array){
+    let sum = 0;
+    for (let obj of array){
+        sum += obj.value
+    }
+    return sum;
+}
